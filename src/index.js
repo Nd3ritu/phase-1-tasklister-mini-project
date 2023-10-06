a931d5e293bee47029923a4f37b00dcfe5cc5559
Matthew <+>
2023-10-06 10:17:55 +0300

    Completed tasklister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    buildToDo(event.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  //console.log(todo);
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "x";
+  p.textContent = `${todo}`;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector("#tasks").appendChild(p);
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove();
+}
